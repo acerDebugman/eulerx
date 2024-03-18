a594c36a64f12880defa5dbe317fb343b499c3a4
acerDebugman <460873250@qq.com>
2024-03-18 20:07:30 +0800

    fix 148

--- leetcode/src/bin/148_sort-list.py
+++ leetcode/src/bin/148_sort-list.py
@@ -26,10 +26,43 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return head
+    def merge2lst(self, h1: Optional[ListNode], h2: Optional[ListNode]) -> Optional[ListNode]:
+        ans = tail = ListNode()
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next = h1
+            else:
+                tail.next = h2
+            h1 = h1.next
+            h2 = h2.next
+            tail = tail.next
         
+        if h1:
+            tail.next = h1
+        if h2:
+            tail.next = h2
+        return ans.next
+
+    # nlogn
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def dfs():
+            pass
+        
+        pass
+    #O(n^2)
+    def sortList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        nxth = self.sortList(head.next)
+        dummyh,p = ListNode(0,nxth),nxth
+        pre = dummyh
+        while p and head.val > p.val:
+            pre = p 
+            p = pre.next
+        head.next = p
+        pre.next = head
+            
+        return dummyh.next
 
 
 if __name__ == "__main__":
